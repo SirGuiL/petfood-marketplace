4ca4d817d06aab8d48752127d9973accc2ed2b07
Guilherme Lima de Souza <guibiel-10@hotmail.com>
2021-10-23 14:10:22 -0300

    front-end finished

---
+++ api/src/database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const URI = "mongodb://localhost/petfoot";
+
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
+mongoose.set("debug", true);
+
+mongoose
+  .connect(URI)
+  .then(() => {
+    console.log("DB is up");
+  })
+  .catch((err) => console.log(err));
